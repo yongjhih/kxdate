685081981a286ebe06a14250f47b6beea41e03bb
Andrew Chen <yongjhih@gmail.com>
2017-04-24 14:06:06 -0700

    Update kxdate.kt

--- kxdate/src/main/kotlin/kxdate.kt
+++ kxdate/src/main/kotlin/kxdate.kt
@@ -7,7 +7,7 @@ import java.time.Period
 
 object ago
 
-object fromNow
+object later
 
 val Int.nanoseconds: Duration
     get() = Duration.ofNanos(toLong())
@@ -42,52 +42,52 @@ val Int.years: Period
 val Duration.ago: LocalDateTime
     get() =  baseTime() - this
 
-val Duration.fromNow: LocalDateTime
+val Duration.later: LocalDateTime
     get() =  baseTime() + this
 
 val Period.ago: LocalDate
     get() = baseDate() - this
 
-val Period.fromNow: LocalDate
+val Period.later: LocalDate
     get() = baseDate() + this
 
-infix fun Int.nanoseconds(fromNow: fromNow) = baseTime().plusNanos(toLong())
+infix fun Int.nanoseconds(later: later) = baseTime().plusNanos(toLong())
 
 infix fun Int.nanoseconds(ago: ago) = baseTime().minusNanos(toLong())
 
-infix fun Int.microseconds(fromNow: fromNow) = baseTime().plusNanos(1000L * toLong())
+infix fun Int.microseconds(later: later) = baseTime().plusNanos(1000L * toLong())
 
 infix fun Int.microseconds(ago: ago) = baseTime().minusNanos(1000L * toLong())
 
-infix fun Int.milliseconds(fromNow: fromNow) = baseTime().plusNanos(1000000L * toLong())
+infix fun Int.milliseconds(later: later) = baseTime().plusNanos(1000000L * toLong())
 
 infix fun Int.milliseconds(ago: ago) = baseTime().minusNanos(1000000L * toLong())
 
-infix fun Int.seconds(fromNow: fromNow) = baseTime().plusSeconds(toLong())
+infix fun Int.seconds(later: later) = baseTime().plusSeconds(toLong())
 
 infix fun Int.seconds(ago: ago) = baseTime().minusSeconds(toLong())
 
-infix fun Int.minutes(fromNow: fromNow) = baseTime().plusMinutes(toLong())
+infix fun Int.minutes(later: later) = baseTime().plusMinutes(toLong())
 
 infix fun Int.minutes(ago: ago) = baseTime().minusMinutes(toLong())
 
-infix fun Int.hours(fromNow: fromNow) = baseTime().plusHours(toLong())
+infix fun Int.hours(later: later) = baseTime().plusHours(toLong())
 
 infix fun Int.hours(ago: ago) = baseTime().minusHours(toLong())
 
-infix fun Int.days(fromNow: fromNow) = baseDate().plusDays(toLong())
+infix fun Int.days(later: later) = baseDate().plusDays(toLong())
 
 infix fun Int.days(ago: ago) = baseDate().minusDays(toLong())
 
-infix fun Int.weeks(fromNow: fromNow) = baseDate().plusWeeks(toLong())
+infix fun Int.weeks(later: later) = baseDate().plusWeeks(toLong())
 
 infix fun Int.weeks(ago: ago) = baseDate().minusWeeks(toLong())
 
-infix fun Int.months(fromNow: fromNow) = baseDate().plusMonths(toLong())
+infix fun Int.months(later: later) = baseDate().plusMonths(toLong())
 
 infix fun Int.months(ago: ago) = baseDate().minusMonths(toLong())
 
-infix fun Int.years(fromNow: fromNow) = baseDate().plusYears(toLong())
+infix fun Int.years(later: later) = baseDate().plusYears(toLong())
 
 infix fun Int.years(ago: ago) = baseDate().minusYears(toLong())
 
